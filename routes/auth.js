5e977bb367f713c0878da3c52aeef6fd5ca24fe1
Yann <yann.jouanique@gmail.com>
2019-03-16 13:34:48 +0700

    Custom login and return URLs

--- routes/auth.js
+++ routes/auth.js
@@ -13,12 +13,12 @@ router.get('/login', passport.authenticate('auth0', {
 router.get('/callback', function (req, res, next) {
   passport.authenticate('auth0', function (err, user, info) {
     if (err) { return next(err); }
-    if (!user) { return res.redirect('/login'); }
+    if (!user) { return res.redirect(process.env.LOGIN_URL || '/login'); }
     req.logIn(user, function (err) {
       if (err) { return next(err); }
       const returnTo = req.session.returnTo;
       delete req.session.returnTo;
-      res.redirect(returnTo || '/user');
+      res.redirect(returnTo || process.env.RETURN_URL || '/user');
     });
   })(req, res, next);
 });
